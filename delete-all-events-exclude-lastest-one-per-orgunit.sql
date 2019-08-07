5bcca9dd4aa34608c52c78a51d5c5130d69c96be
ibrahimwickama <ibrahimwickama@gmail.com>
2019-08-07 13:04:48 +0300

    feat: rectify deletion of events keeping last one only per orgunit

--- delete-all-events-exclude-lastest-one-per-orgunit.sql
+++ delete-all-events-exclude-lastest-one-per-orgunit.sql
@@ -1,17 +1,29 @@
   -- SQL Queries
-DELETE FROM trackedentitydatavalue WHERE programstageinstanceid NOT IN(SELECT programstageinstanceid FROM programstageinstance
-WHERE executiondate >= '2018-01-01' AND executiondate < '2020-01-01' AND organisationunitid = 2479
-GROUP BY organisationunitid, programstageinstanceid
-ORDER BY lastupdated DESC LIMIT 1)
-AND programstageinstanceid IN(SELECT programstageinstanceid FROM programstageinstance
-WHERE executiondate >= '2018-01-01' AND executiondate < '2020-01-01' AND organisationunitid = 2479
-GROUP BY organisationunitid, programstageinstanceid
-ORDER BY lastupdated DESC);
+DELETE FROM trackedentitydatavalue WHERE programstageinstanceid NOT IN(SELECT programstageinstanceid FROM programstageinstance psi
+INNER JOIN programstage ps ON(ps.programstageid = psi.programstageid AND ps.uid = 'TfElxurgcBm')
+WHERE psi.executiondate >= '2018-01-01' AND psi.executiondate < '2020-01-01' AND psi.organisationunitid = 2479
+GROUP BY psi.organisationunitid, psi.programstageinstanceid
+ORDER BY psi.lastupdated DESC LIMIT 1)
+AND programstageinstanceid IN(SELECT programstageinstanceid FROM programstageinstance psi2
+WHERE psi2.executiondate >= '2018-01-01' AND psi2.executiondate < '2020-01-01' AND psi2.organisationunitid = 2479
+GROUP BY psi2.organisationunitid, psi2.programstageinstanceid
+ORDER BY psi2.lastupdated DESC);
 
-DELETE FROM programstageinstance WHERE programstageinstanceid NOT IN(SELECT programstageinstanceid FROM programstageinstance
-WHERE executiondate >= '2018-01-01' AND executiondate < '2020-01-01' AND organisationunitid = 2479 
-GROUP BY organisationunitid, programstageinstanceid
-ORDER BY lastupdated DESC LIMIT 1
+DELETE FROM trackedentitydatavalueaudit WHERE programstageinstanceid NOT IN(SELECT programstageinstanceid FROM programstageinstance psi
+INNER JOIN programstage ps ON(ps.programstageid = psi.programstageid AND ps.uid = 'TfElxurgcBm')
+WHERE psi.executiondate >= '2018-01-01' AND psi.executiondate < '2020-01-01' AND psi.organisationunitid = 2479
+GROUP BY psi.organisationunitid, psi.programstageinstanceid
+ORDER BY psi.lastupdated DESC LIMIT 1)
+AND programstageinstanceid IN(SELECT programstageinstanceid FROM programstageinstance psi2
+WHERE psi2.executiondate >= '2018-01-01' AND psi2.executiondate < '2020-01-01' AND psi2.organisationunitid = 2479
+GROUP BY psi2.organisationunitid, psi2.programstageinstanceid
+ORDER BY psi2.lastupdated DESC);
+
+DELETE FROM programstageinstance WHERE programstageinstanceid NOT IN(SELECT programstageinstanceid FROM programstageinstance psi
+INNER JOIN programstage ps ON(ps.programstageid = psi.programstageid AND ps.uid = 'TfElxurgcBm')
+WHERE psi.executiondate >= '2018-01-01' AND psi.executiondate < '2020-01-01' AND psi.organisationunitid = 2479 
+GROUP BY psi.organisationunitid, psi.programstageinstanceid
+ORDER BY psi.lastupdated DESC LIMIT 1
 ) AND organisationunitid = 2479;
 
 
@@ -22,23 +34,29 @@ DECLARE
     rec   record;
 BEGIN
    FOR rec IN
-      SELECT organisationunitid FROM organisationunit
+      SELECT organisationunitid FROM organisationunit WHERE hierarchylevel = 4
    LOOP 
-        DELETE FROM trackedentitydatavalue WHERE programstageinstanceid NOT IN(SELECT programstageinstanceid FROM programstageinstance
-        WHERE executiondate >= '2018-01-01' AND executiondate < '2020-01-01' AND organisationunitid::TEXT = rec::TEXT
+        DELETE FROM trackedentitydatavalueaudit WHERE programstageinstanceid IN(SELECT programstageinstanceid FROM programstageinstance
+         WHERE programstageinstanceid NOT IN(SELECT programstageinstanceid FROM programstageinstance psi
+        WHERE executiondate >= '2018-01-01' AND executiondate < '2020-01-01' AND organisationunitid = rec.organisationunitid
         GROUP BY organisationunitid, programstageinstanceid
-        ORDER BY lastupdated DESC LIMIT 1)
-        AND programstageinstanceid IN(SELECT programstageinstanceid FROM programstageinstance
-        WHERE executiondate >= '2018-01-01' AND executiondate < '2020-01-01' AND organisationunitid::TEXT = rec::TEXT
+        ORDER BY lastupdated DESC LIMIT 1
+        ) AND organisationunitid = rec.organisationunitid);
+
+        DELETE FROM trackedentitydatavalue WHERE programstageinstanceid IN(SELECT programstageinstanceid FROM programstageinstance
+        WHERE programstageinstanceid NOT IN(SELECT programstageinstanceid FROM programstageinstance psi
+        WHERE executiondate >= '2018-01-01' AND executiondate < '2020-01-01' AND organisationunitid = rec.organisationunitid
         GROUP BY organisationunitid, programstageinstanceid
-        ORDER BY lastupdated DESC);
+        ORDER BY lastupdated DESC LIMIT 1
+        ) AND organisationunitid = rec.organisationunitid);
 
         DELETE FROM programstageinstance WHERE programstageinstanceid NOT IN(SELECT programstageinstanceid FROM programstageinstance psi
-        INNER JOIN programstage ps ON(ps.programstageid = psi.programstageid AND ps.uid = 'TfElxurgcBm')
-        WHERE psi.executiondate >= '2018-01-01' AND psi.executiondate < '2020-01-01' AND psi.organisationunitid::TEXT = rec::TEXT
-        GROUP BY psi.organisationunitid, psi.programstageinstanceid
-        ORDER BY psi.lastupdated DESC LIMIT 1
-        ) AND organisationunitid::TEXT = rec::TEXT;
+        WHERE executiondate >= '2018-01-01' AND executiondate < '2020-01-01' AND organisationunitid = rec.organisationunitid
+        GROUP BY organisationunitid, programstageinstanceid
+        ORDER BY lastupdated DESC LIMIT 1
+        ) AND organisationunitid = rec.organisationunitid;
+
+        raise notice 'Deleted on organisationunitid: %', rec.organisationunitid;
 
    END LOOP;
 END
